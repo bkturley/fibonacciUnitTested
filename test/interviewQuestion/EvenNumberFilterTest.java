483c8759a51a8078fab1fb7732449c6f5dc83df6
bkturley <kyle@kturley.com>
2015-01-11 10:04:46 -0600

    Added tests for EvenNumberFilter.java

---
+++ test/interviewQuestion/EvenNumberFilterTest.java
@@ -0,0 +1,74 @@
+package interviewQuestion;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author bkturley
+ */
+public class EvenNumberFilterTest {
+
+    public EvenNumberFilterTest() {
+    }
+
+    @Test
+    public void testGetEvenNumbersFromList_0To999999() {
+        List<Integer> getMyEvenNumbers = getList0To999999();;
+
+        EvenNumberFilter instance = new EvenNumberFilter();
+        List expResult = getEvenNumbersFromList(getMyEvenNumbers);
+        List result = instance.getEvenNumbersFromList(getMyEvenNumbers);
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testGetEvenNumbersFromList_Neg999999To999999() {
+        List<Integer> getMyEvenNumbers = getListNeg999999To999999();;
+        EvenNumberFilter evenNumberFilter = new EvenNumberFilter();
+        List expResult = getEvenNumbersFromList(getMyEvenNumbers);
+        List result = evenNumberFilter.getEvenNumbersFromList(getMyEvenNumbers);
+        assertEquals(expResult, result);
+    }
+    
+    
+    private List<Integer> getEvenNumbersFromList(List getMyEvens) {
+        List<Integer> returnMe = new ArrayList<>();
+        Iterator<Integer> iterator = getMyEvens.iterator();
+        while(iterator.hasNext()){
+            Integer currentValue = iterator.next();
+            boolean counterIsOddNumber = (currentValue % 2 == 0);
+            if (counterIsOddNumber) {
+                returnMe.add(currentValue);
+            }
+        }
+        return returnMe;
+    }
+    
+    private List<Integer> getList0To999999() {
+        List<Integer> returnMe = new ArrayList<>();
+        for (int i = 0; i < 999999; i++) {
+            returnMe.add(i);
+        }
+        return returnMe;
+    }
+    
+    private List<Integer> getListNeg999999To999999() {
+        List<Integer> returnMe = new ArrayList<>();
+        for (int i = -999999; i < 999999; i++) {
+            returnMe.add(i);
+        }
+        return returnMe;
+    }
+
+
+}
